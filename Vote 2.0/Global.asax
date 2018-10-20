a0a64109d6cc8b8cdd8acae4d004c7797f3d540f
Yamil Burgos <yamil.burgos1293@gmail.com>
2018-10-20 14:07:32 -0400

    Add project files.

---
+++ Vote 2.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vote_2._0.MvcApplication" Language="C#" %>
---
+++ Vote 2.0/Models/Enums/GovernmentLevel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vote_2._0.Models.Enums
+{
+	public class GovernmentLevel
+	{
+	}
+}
